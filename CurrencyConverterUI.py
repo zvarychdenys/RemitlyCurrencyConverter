10cd02df12b24cc083c8b0a3451acef88ce9f2d4
Denys Zvarych <denys.zvr@gmail.com>
2023-02-17 01:08:15 +0100

    Updated code with additional comments to improve readability and understanding

--- CurrencyConverterUI.py
+++ CurrencyConverterUI.py
@@ -7,14 +7,14 @@ from parserNbpAPI import CurrentExchangeRates, CurrencyConverter, CurrencyLastRa
 class StreamlitPageUI():
 
     def __init__(self) -> None:
-
+        # ustawienia strony Streamlit
         st.set_page_config(
             page_title = 'Remitly currency converter',
             page_icon="🏦",
             layout = "centered"
         )
 
-        # Hide Streamlit menu and footer
+        # ukrycie menu i stopki Streamlit.
         hide_streamlit_style = """
         <style>
         #MainMenu {visibility: hidden;}
@@ -23,20 +23,25 @@ class StreamlitPageUI():
         """
         st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
-        # Disable CORS and other Streamlit settings
+        # wyłącenie CORS i inne ustawienia Streamlit
         config.set_option('server.enableCORS', False)
         config.set_option('global.developmentMode', False)
         config.set_option('server.useWebsocketCompression', False)
 
+        # nagłówek strony
         st.markdown("<h1 style='text-align: center;'>Remitly currency converter</h1>", unsafe_allow_html=True)
 
+        # wczytanie animacji
         image = self.load_lottieurl()
         st_lottie(image, width=None, height=400, key="ImageIllustration")
 
+        # tytuł sekcji
         st.markdown("<h2 style='text-align: center;'>Where would you like to send money?</h2>", unsafe_allow_html=True)
 
     
     def load_lottieurl(self):
+        '''Wczytanie animacji z serwera https://lottiefiles.com/'''
+
         getImage = requests.get('https://assets7.lottiefiles.com/packages/lf20_2AFtKRR44D.json')
         if getImage.status_code != 200:
             return None
@@ -45,43 +50,54 @@ class StreamlitPageUI():
 
 
 class RemitlyCurrencyConverter(CurrentExchangeRates):
+    '''Klasa dziedzicząca po CurrentExchangeRates, która implementuje przelicznik walut'''
     
     def __init__(self) -> None:
         with st.form("You send"):
-
+            
+            # pobranie od użytkownika kwoty do przeliczenia
             self.amount = st.number_input("Enter amount to convert:", min_value = .25, step = .25)
+
+            # tworzenie dwóch rozwijanych list z nazwami walut
             self.select_box1, self.select_box2 = st.columns(2)
             self.select_box1.selectbox('You send', currency_codes, key='send')
             self.select_box2.selectbox('They receive', ['PLN'], key = 'receive')
 
+            # utworzenie przycisku "Send now", który spowoduje przesłanie formularza.
             self.submitted = st.form_submit_button("Send now")
 
-            self.press_buttons_test()
+            self.handle_submit()
 
     def currency_converter(self, amount, from_currency, to_currency):
         
         self.amount  = amount
         self.currency  = from_currency
-    
+
+        # utworzenie obiektu CurrencyConverter, który służy do pobierania kursu waluty w danym dniu
         self.converter = CurrencyConverter(self.currency)
         return round(self.converter.getCurrencyToPLN() * self.amount, 2)
 
     def calculate_percentage(self,code):
         
+        # utworzenie obiektu CurrencyLastRates, który pobiera kursy walut z ostatnich 30 dni.
         currency_data = CurrencyLastRates(code, days=30)
         curenncy_df = currency_data.getCurrencyData()
 
         curenncy_df.sort_values(by = 'effectiveDate', inplace=True, ascending=False)
         curenncy_df.reset_index(drop=True, inplace=True)
 
+        # obliczenie różnicy między kursem waluty z dzisiejszego dnia a kursami z ostatnich 30 dni
         self.percentage_diff = round((curenncy_df.iloc[0]['mid'] - curenncy_df.iloc[-1]['mid'])/ curenncy_df.iloc[-1]['mid'] * 100,2)
         return str(self.percentage_diff) + ' %'
 
     def display_graph(self):
-
+        
         plot = CurrencyLastRates(st.session_state['send'], days=30)
+
+        # tworzenie wykresu w postaci linii wykresu z danych z obiektu plot.
         plot_line_graph = plot.historicalExchangeRates()
 
+        # ustawienie szerokości, wysokości oraz marginesów wykresu.
         plot_line_graph.update_layout(
             width=675,
             height=450,
@@ -90,13 +106,16 @@ class RemitlyCurrencyConverter(CurrentExchangeRates):
 
         st.plotly_chart(plot_line_graph)
 
-    def press_buttons_test(self):
+    def handle_submit(self):
+        '''Funkcja obsługująca naciśnięcie przycisku formularza'''
 
-        if self.submitted:
+        if self.submitted: # sprawdzenie czy przycisk został naciśnięty
             self.result = self.currency_converter(self.amount, st.session_state['send'], st.session_state['receive'])
-            self.rounded_result = round(self.result)
+            self.rounded_result = round(self.result,2)
+            
+            # wyświetlenia wyników przeliczenia
             st.write(f"1 {st.session_state['send']} = {round(self.result/self.amount,2)} {(st.session_state['receive'])}")
-            st.success(f"{round(self.amount,2)} {st.session_state['send']} is equal to { self.rounded_result} {st.session_state['receive']} 🇵🇱")
+            st.success(f"{round(self.amount,2)} {st.session_state['send']} is equal to {self.rounded_result} {st.session_state['receive']} 🇵🇱")
 
             st.metric(
                 label = f'The value of the **{self.converter.getCurrencyName().capitalize()}** changed over the last 30 days to PLN:', 
@@ -106,16 +125,17 @@ class RemitlyCurrencyConverter(CurrentExchangeRates):
 
             st.markdown("<h2 style='text-align: center;'>Analysis of the currency trends over the past 30 days</h2>", unsafe_allow_html=True)
 
+            # wyświetlenia wykresu z trendem walutowym.
             self.display_graph()
 
 
-t = StreamlitPageUI()
+sreamlit_ui = StreamlitPageUI()
+exchange_rates = CurrentExchangeRates()
 
-all_data_rates = CurrentExchangeRates()
-currency_names = all_data_rates.getCurrencyNames()
-currency_codes = all_data_rates.getCurrencyCodes()
+currency_names = exchange_rates.getCurrencyNames()
+currency_codes = exchange_rates.getCurrencyCodes()
 
-t2 = RemitlyCurrencyConverter()
+currency_converter = RemitlyCurrencyConverter()
 
 
     
--- parserNbpAPI.py
+++ parserNbpAPI.py
@@ -1,33 +1,33 @@
-import requests
-import pandas as pd
-import matplotlib.pyplot as plt
-import numpy as np
-import plotly.express as px
-
-
+import requests # wykonywania zapytań HTTP
+import pandas as pd # przetwarzania danych
+import plotly.express as px # do tworzenia wykresów
 
 class CurrencyConverter():
-
+    ''''Przeliczanie aktualnej ceny waluty na złotówki,
+    przyjmuje kod waluty jako argument i korzysta z NBP  API do pobrania informacji o 
+    kursie waluty w formacie JSON'''
+    
     def __init__(self, code) -> None:
-        self.code = code # trzyliterowy kod waluty (standard ISO 4217)
-        self.url = f'http://api.nbp.pl/api/exchangerates/rates/a/{self.code}/?format=json'
+        self.code = code # trzyliterowy kod waluty 
+        self.url = f'http://api.nbp.pl/api/exchangerates/rates/a/{self.code}/?format=json' 
 
         self.request = requests.get(self.url)
     
         try:
-            self.currencyInfo = self.request.json()
-
+            self.currencyInfo = self.request.json() # pobieranie kursów walut w formacie json
         except Exception as ex: 
             print(f'Niestety nie można znaleźć danego kursu {self.code.upper()} dla PLN: {ex}')
 
-    
     def getCurrencyToPLN(self) -> float:
-        return self.currencyInfo['rates'][0]['mid'] # wyciagam aktualna cene
+        return self.currencyInfo['rates'][0]['mid'] # wyciągnięcie aktualnej ceny
 
     def getCurrencyName(self) -> str:
-        return self.currencyInfo['currency'] # wyciagam nazwe waluty
+        return self.currencyInfo['currency'] # odczytanie nazwy waluty
 
 class CurrentExchangeRates():
+
+    '''Umożliwia użytkownikom pobranie listy nazw i kodów 
+    wszystkich walut dostępnych w API NBP.'''
 
     def __init__(self) -> None:
         self.url = 'http://api.nbp.pl/api/exchangerates/tables/a/'
@@ -35,12 +35,11 @@ class CurrentExchangeRates():
 
         try:
             self.data = request.json()        
-        except Exception as ex: 
+        except Exception as ex:  # w przypadku braku połączenia z API wyświetlam informację o błędzie.
             print(f'Niestety nie można znaleźć aktualnych kursow')
 
-
     def getCurrencyNames(self):
-         
+        '''Zwraca listę nazw walut dostępnych w API NBP, nazwy walut są zwracane z pierwszą literą dużą'''
         self.currencies = []
 
         for currency in self.data[0]['rates']:
@@ -50,7 +49,7 @@ class CurrentExchangeRates():
         return self.currencies
 
     def getCurrencyCodes(self):
-         
+        '''Zwraca listę kodów walut dostępnych w API NBP'''
         self.codes = []
 
         for currency in self.data[0]['rates']:
@@ -64,41 +63,44 @@ class CurrentExchangeRates():
 
 
 class CurrencyLastRates():
+    '''Umożliwia użytkownikom pobranie informacji o kursach wybranej waluty w określonym okresie czasu,
+     przyjmuje kod waluty oraz liczbę dni, dla których użytkownik chce pobrać informacje o kursach'''
 
-        def __init__(self, code, days=30) -> None:
-            self.code = code # trzyliterowy kod waluty (standard ISO 4217)
-            self.days = days # za ile ostatnich dni chcemy wiedziec informacje o walucie. default - 10, max - 255
+    def __init__(self, code, days=30) -> None:
+        self.code = code # trzyliterowy kod waluty 
+        self.days = days # ile ostatnich dni chcesz uwzględnić w informacjach o kursach wybranej waluty, default - 30, max - 255
 
-            self.url = f'http://api.nbp.pl/api/exchangerates/rates/a/{code}/last/{days}/?format=json'
-            #  self.url = f'http://api.nbp.pl/api/exchangerates/rates/a/{code}/last/{int(days.split(" ")[0])}/?format=json'
+        self.url = f'http://api.nbp.pl/api/exchangerates/rates/a/{code}/last/{days}/?format=json'
 
-            self.request = requests.get(self.url)
-        
-            try:
-                self.currencyData = self.request.json()
-                self.currencyDataFrame = pd.DataFrame(self.currencyData['rates'])
-                self.currencyDataFrame.drop(columns='no', inplace=True)
-            except Exception as ex: 
-                print(f'Niestety nie można znaleźć danego kursu {self.code.upper()} dla PLN: {ex}')
-
-        def getCurrencyData(self):
-            
-            # self.currencyDataFrame = pd.DataFrame(self.currencyData['rates']).set_index('effectiveDate')
+        self.request = requests.get(self.url)
+    
+        try:
+            self.currencyData = self.request.json()
             self.currencyDataFrame = pd.DataFrame(self.currencyData['rates'])
             self.currencyDataFrame.drop(columns='no', inplace=True)
-            return self.currencyDataFrame 
-        
-        def historicalExchangeRates(self):
-            currencyName = self.currencyData['currency'] # wyciagam imie waluty z json pliku
-            currencyCode = self.currencyData['code'] # wyciagam cod waluty z json pliku
+        except Exception as ex: 
+            print(f'Niestety nie można znaleźć danego kursu {self.code.upper()} dla PLN: {ex}')
 
-            plotCurrencyRates = px.line(x=self.currencyDataFrame['effectiveDate'], 
-                y=self.currencyDataFrame['mid'], 
-                labels={'x': 'Data', 'y': f'Exchange Rates ({currencyCode})'}, 
-                title=f'Kursy waluty {currencyName.capitalize()} w czasie'
-            )
+    def getCurrencyData(self):
+        '''Zwraca DataFrame z kursami waluty w danym przedziale czasowym.'''
 
-            return plotCurrencyRates
+        self.currencyDataFrame = pd.DataFrame(self.currencyData['rates'])
+        self.currencyDataFrame.drop(columns='no', inplace=True)
+        return self.currencyDataFrame
+    
+    def historicalExchangeRates(self):
+        '''Tworzy wykres liniowy przedstawiający zmiany kursu waluty w czasie.'''
+
+        currencyName = self.currencyData['currency']  # wyciąganie nazwy waluty z JSON
+        currencyCode = self.currencyData['code']  # wyciąganie kodu waluty z JSON
+
+        plotCurrencyRates = px.line(x=self.currencyDataFrame['effectiveDate'], 
+            y=self.currencyDataFrame['mid'], 
+            labels={'x': 'Data', 'y': f'Exchange Rates ({currencyCode})'}, 
+            title=f'Kursy waluty {currencyName.capitalize()} w czasie'
+        )
+
+        return plotCurrencyRates
 
 
 
